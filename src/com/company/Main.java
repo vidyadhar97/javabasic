4a1237bd58f8724feae3225635543285afa5500a
unknown <vidyadharkotyal@gmail.com>
2020-10-02 16:38:09 +0530

    Vid|Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,19 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-      double r = 10 ;
-      double circumference = (double) (2 * 3.14 * r );
-      System.out.println(circumference);
-      double area =(double) (3.14* r * r);
-      System.out.print(area);
+
+        int[] radiusList = new int[]{1, 2, 3, 4, 5};
+        for (int i = 0; i <= 4; i++) {
+            int radius = radiusList[i];
+            double circumference = (2 * 3.14 * radius);
+            System.out.println("Circumference for radius "+ radius+" is " + String.format("%.2f",circumference));
+            double area = 3.14 * radius * radius;
+            System.out.println("Area for " + radius+" is "+ area);
+        }
     }
     }
