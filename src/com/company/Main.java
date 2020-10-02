e0963d50b78c9903f22ae4dfbf853299a4d0d883
unknown <vidyadharkotyal@gmail.com>
2020-10-02 15:02:35 +0530

    Vid| First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       int age = 26;
+       int minimunVotingAge = 18;
+       if(minimunVotingAge<= shitalAge )
+           System.out.println("Shital Age is Qualified for voting");
+       else
+           System.out.println("Age not Qualified");
+    }
+}
