ab32c208084e9dd57e02966ce6084292d25cf073
unknown <vidyadharkotyal@gmail.com>
2020-10-04 12:04:42 +0530

    VID|nested looping

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] radiusList = new int[]{1, 2, 3, 4, 5};
-        for (int i = 0; i <= 4; i++) {
-            int radius = radiusList[i];
-            double circumference = (2 * 3.14 * radius);
-            System.out.println("Circumference for radius "+ radius+" is " + String.format("%.2f",circumference));
-            double area = 3.14 * radius * radius;
-            System.out.println("Area for " + radius+" is "+ area);
+        for (int i = 0; i <= 10; i++) {
+
+            if(i%2 == 0) {
+                System.out.println("No " + i + " is even");
+            }
+                else
+                {
+                    System.out.println("No " + i + " is odd");
+                }
+
+
         }
     }
     }
